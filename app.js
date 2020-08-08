b208032f664a9c5909d27589b33929503be4a0b0
Louis Savoie <thelousavoie@gmail.com>
2020-08-07 23:56:19 -0400

    add create comments and button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express    = require("express"),
       app        = express(),
       mongoose   = require('mongoose'),
       Campground = require("./models/campground"),
+      Comment    = require("./models/comment"),
       seedDB     = require("./seeds");
 
 seedDB();
@@ -19,7 +20,9 @@ mongoose.connect('mongodb://localhost/yelpcamp', {
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// ROUTES
+// ===================
+// CAMPGROUNDS ROUTES
+// ===================
 
 // ROOT
 app.get("/", function(req, res){
@@ -33,7 +36,7 @@ app.get("/campgrounds", function(req, res){
         if (err) {
             console.log("Error reading campgrounds from DB: " + err);
         } else {
-            res.render("index", {campgrounds: allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
         }
     });
 });
@@ -62,7 +65,7 @@ app.post("/campgrounds", function(req, res){
 
 // NEW
 app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // SHOW
@@ -73,7 +76,48 @@ app.get("/campgrounds/:id", function(req, res){
             console.log(err);
         } else {
             // render template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+// ===============
+// COMMENT ROUTES
+// ===============
+
+// NEW COMMENT
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            // render the new comment form
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+// CREATE COMMENT
+app.post("/campgrounds/:id/comments", function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+          // create new comment
+          Comment.create(req.body.comment, function(err, comment){
+            if(err){
+                console.log(err);
+            } else {
+                // connect new comment to campground
+                campground.comments.push(comment);
+                campground.save();
+                // redirect to campground show page
+                res.redirect("/campgrounds/" + campground._id);
+            }
+          });
         }
     });
 });
