a62f45ca9ede00a0a0a819c280ba1aea97b8aa04
Louis Savoie <thelousavoie@gmail.com>
2020-08-08 23:01:18 -0400

    setpu passport for auth

--- app.js
+++ app.js
@@ -24,6 +24,18 @@ app.use(express.static(__dirname + "/public"));
 
 seedDB();
 
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Hello, Gordon! Another day another dollar, am I right? Hahaha.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize);
+app.use(passport.session);
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // ===================
 // CAMPGROUNDS ROUTES
 // ===================
