948578483db2a3e2c4c47aa54d2d05db3ea62c89
Louis Savoie <thelousavoie@gmail.com>
2020-08-14 00:25:45 -0400

    seedDB: add comments loop / fix campground author id

--- seeds.js
+++ seeds.js
@@ -15,8 +15,9 @@ async function seedDB(){
     await User.deleteMany({});
     console.log("Seed: Users Removed");
     // Create User
+    let newUser = new User({username: "testbot"});
     let password = "testy!";
-    await User.register({username: "testbot"}, password);
+    await User.register(newUser, password);
     console.log("Seed: User(testbot) Created");
     let testbot = await User.findOne({username: "testbot"});
 
@@ -27,36 +28,51 @@ async function seedDB(){
             image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
             price: "5",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            author: testbot
+            author: {id: testbot._id, username: testbot.username}
         },
         {
             name: "Desert Mesa", 
             image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
             price: "12",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            author: testbot
+            author: {id: testbot._id, username: testbot.username}
         },
         {
             name: "Canyon Floor", 
             image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
             price: "20",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            author: testbot
+            author: {id: testbot._id, username: testbot.username}
         }
     ];
 
+    // Comments Data (I know this is not good practice, it's just for testing.)
+    let commentSeeds = [
+        {
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            author: {id: testbot._id, username: testbot.username}
+        },
+        {
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            author: {id: testbot._id, username: testbot.username}
+        },
+        {
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            author: {id: testbot._id, username: testbot.username}
+        }
+    ]
+
     // Create Campgrounds with Comments by User
-    for(const seed of campSeeds){
+    for(const seedCamp of campSeeds){
         // Create Campground
-        let campground = await Campground.create(seed);
+        let campground = await Campground.create(seedCamp);
         console.log("Seed: Campground Created");
-        // Create Comment
-        let comment = await Comment.create({
-                text: "This place is great, but I wish it had internet.",
-                author: testbot
-            });
-        console.log("Seed: Comment Created");
-        campground.comments.push(comment);
+        for(const seedComment of commentSeeds){
+            // Create Comment
+            let comment = await Comment.create(seedComment);
+            console.log("Seed: Comment Created");
+            campground.comments.push(comment);
+        }
         campground.save();
         console.log("Seed: Comment added to Campground");
     }
